2f73f3a035f714094e97a757feb243f2caab74e9
Dario <darioms511@gmail.com>
2020-05-20 17:52:01 +0200

    Cambiar atributo estado a protected

--- Facturas/src/Facturas.java
+++ Facturas/src/Facturas.java
@@ -2,12 +2,12 @@ public class Facturas implements FacturasI {
 	public int num;
         public float base;
         public float tipoIva;
-	public Estados estado;
+	protected Estados estado;
 
 	public static void main(String[] args){
 		
 	}
-	void cambiarEstado(Esatdos estado){};
-	public float  getTotal(){};
-	public  float getCantidadIva(){};
+	void cambiarEstado(Esatdos estado){}
+	public float  getTotal(){}
+	public  float getCantidadIva(){}
 }
